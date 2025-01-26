a7c9cb4022c4724aa4af82bb4bb3683b2b5de1c4
RealBoy <nallabothulanagarjuna0@gmail.com>
2025-01-26 13:31:09 +0530

    Removed eureka registration

--- src/main/java/com/epam/e_commerce/client/CartClient.java
+++ src/main/java/com/epam/e_commerce/client/CartClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "CART-MANAGEMENT")
+@FeignClient(name = "CART-MANAGEMENT" ,url = "https://ecommerce-cart-service.onrender.com")
 public interface CartClient {
 	@DeleteMapping("/cart/{productId}")
 	void deleteByProductIdFromCart(@PathVariable("productId") Integer productId);
